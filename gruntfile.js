d4d75455e8949101c832f100668f03e4805133e9
Peter Andreas Moelgaard <gitkraken@petermolgaard.com>
2019-08-08 23:06:45 +0200

    now with logo and style

--- gruntfile.js
+++ gruntfile.js
@@ -1,58 +1,75 @@
-module.exports = function(grunt) {
-  "use strict";
+module.exports = function (grunt) {
+    "use strict";
 
-  grunt.initConfig({
-    copy: {
-      build: {
-        files: [
-          {
-            expand: true,
-            cwd: "./public",
-            src: ["**"],
-            dest: "./dist/public"
-          },
-          {
-            expand: true,
-            cwd: "./views",
-            src: ["**"],
-            dest: "./dist/views"
-          }
-        ]
-      }
-    },
-    ts: {
-      app: {
-        files: [{
-          src: ["src/**/*.ts", "!src/.baseDir.ts"],
-          dest: "./dist"
-        }],
-        options: {
-          module: "commonjs",
-          target: "es6",
-          sourceMap: false,
-          rootDir: "src"
+    grunt.initConfig({
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "./public",
+                        src: ["**"],
+                        dest: "./dist/public"
+                    },
+                    {
+                        expand: true,
+                        cwd: "./views",
+                        src: ["**"],
+                        dest: "./dist/views"
+                    }
+                ]
+            }
+        },
+        ts: {
+            app: {
+                files: [{
+                    src: ["src/**/*.ts", "!src/.baseDir.ts"],
+                    dest: "./dist"
+                }],
+                options: {
+                    module: "commonjs",
+                    target: "es6",
+                    sourceMap: false,
+                    rootDir: "src"
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/styles',
+                    src: ['*.scss'],
+                    dest: 'dist/styles',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            ts: {
+                files: ["src/**/*.ts"],
+                tasks: ["ts"]
+            },
+            views: {
+                files: ["views/**/*.pug"],
+                tasks: ["copy"]
+            },
+            sass: {
+                files: ["src/styles/*.scss"],
+                tasks: ["sass"]
+            }
         }
-      }
-    },
-    watch: {
-      ts: {
-        files: ["src/**/*.ts"],
-        tasks: ["ts"]
-      },
-      views: {
-        files: ["views/**/*.pug"],
-        tasks: ["copy"]
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks("grunt-ts");
 
-  grunt.registerTask("default", [
-    "copy",
-    "ts"
-  ]);
+    grunt.registerTask("default", [
+        "copy",
+        "ts",
+        "sass"
+    ]);
 
 };
